30a2f329a958d75b1384d35ef9387506841b5f40
bd391105 <bd391105@C39473.ens.uqam.ca>
2014-09-24 11:08:51 -0300

    Premier commit instance2

--- src/instance1/Instance1.java
+++ src/instance1/Instance1.java
@@ -17,7 +17,7 @@ public class Instance1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("allo");
+        System.out.println("hey");
     }
     
 }
